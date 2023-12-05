24df1eed7c6610837c2ba25dbd57594f3d496bdb
reggyshicky <reginahshikanda@gmail.com>
2023-12-05 23:58:45 +0300

    methods in java, toLowerCase, toUpperCsse, isBlank

--- Java_Foundations/src/Main.java
+++ Java_Foundations/src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java ");
+        String home = "Lunza";
+        System.out.println(home.toUpperCase());
+        System.out.println(home.toLowerCase());
+        System.out.println(home.substring(3)); //za
+        System.out.println("  ".isEmpty()); //false
+        System.out.println("  ".isBlank()); //true
+        System.out.println("hello java ");
+        System.out.println(10 + 10);
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+
     }
 }
