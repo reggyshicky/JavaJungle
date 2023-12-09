406347659a8790f8d8b7a612cf15f60dd5feb3f7
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-09 19:55:25 +0300

    System.out.println

---
+++ SECTION ONE/workbook 1.1/FirstProgram.java
@@ -0,0 +1,10 @@
+//package workbook 1.1;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Printing is fun!");
+        System.out.println("Python > Python.");
+        System.out.println("I spilled Java all over my paper");
+        System.out.println("My dog ate my java");
+    }
+}
