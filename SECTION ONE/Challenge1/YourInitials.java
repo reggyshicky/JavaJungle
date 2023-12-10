994b16e6979a35df9cbe96ce096e46101ffac773
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-10 18:56:30 +0300

    print letter initials

---
+++ SECTION ONE/Challenge1/YourInitials.java
@@ -0,0 +1,16 @@
+//package SECTION ONE.Challenge1;
+
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("PPPP      SSS   ");
+        System.out.println("P   P    S   S  ");
+        System.out.println("P   P    S      ");
+        System.out.println("PPPP      SSS   ");
+        System.out.println("P             S  ");
+        System.out.println("P        S    S  ");
+        System.out.println("P         SSS    ");
+
+
+    }
+    
+}
