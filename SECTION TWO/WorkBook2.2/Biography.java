758d23d225b35b0e9273fdd50e50cdca2612e667
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-11 23:51:49 +0300

    a char in java occupies 2 bytes in memory, this is strange to me

---
+++ SECTION TWO/WorkBook2.2/Biography.java
@@ -0,0 +1,21 @@
+//package WorkBook2.2;
+
+public class Biography {
+    public static void main(String[] args) {
+        String name = "Reginah";
+        int age = 24;
+        String country = "Kenya";
+        String sport = "Car Racing";
+        int hours = 5;
+        String game = "BadMinton";
+        String subject = "Java & C#";
+        char grade = 'A';
+
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country + " .");
+        System.out.println("My favourite sport is " + sport + ". I play for " + hours + " hours a day");
+        System.out.println("When I'm tired, I like to play " +  game + ".");
+        System.out.println("In school, my favourite subject was " + subject + ", I scored an " + grade + ".");
+
+
+    }
+}
