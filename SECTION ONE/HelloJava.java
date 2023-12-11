4e42244b0db18b4380e4f6e86fbb9ce4373c3a7c
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-11 22:44:04 +0300

    string concatenation using the plus

--- SECTION ONE/HelloJava.java
+++ SECTION ONE/HelloJava.java
@@ -3,5 +3,6 @@
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        System.out.println("I love Java");
     }
 }
---
+++ SECTION TWO/JoiningStrings.java
@@ -0,0 +1,10 @@
+//package Workbook2.1;
+
+public class JoiningStrings {
+    public static void main(String[] args) {
+        int year = 2010;
+        String winner = "Spain";
+        String announcements = "The winner of the " + year + " world cup are " + winner;
+        System.out.println(announcements);
+     }
+}
