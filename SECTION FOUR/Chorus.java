2d19b577c36eeb770e6706212d3917a6977941ad
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-16 09:58:37 +0300

    functions in java

---
+++ SECTION FOUR/Chorus.java
@@ -0,0 +1,13 @@
+public class Chorus {
+    public static void main(String[] args) {
+        singChorus();
+    }
+    public static void singChorus() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Don't blame it on the sunshine");
+            System.out.println("Don't blame it on the moonlight");
+            System.out.println("Don't blame it on good times");
+            System.out.println("Don't blame it on the boogie\n");
+        }
+    }
+}
