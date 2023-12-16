4ef96fe2e93386366a4a65b957310e0c46ad2b17
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-16 23:26:02 +0300

    functions in java

--- SECTION FOUR/Parameter.java
+++ SECTION FOUR/Parameter.java
@@ -1,9 +1,37 @@
 public class Parameter {
     public static void main(String[] args) {
-        System.out.println(calculateArea(5.2, 10.4 ));
+        double area = calculateArea(2.3, 3.6);
+        printArea(2.3, 3.6, area);
+        String englishExplanation = explainArea("English");
+        System.out.println(englishExplanation);
     }
     public static double calculateArea(double length, double width) {
+        if (length < 0 || width < 0) {
+            System.out.println("Invalid dimensions");
+            System.exit(0); //terminate the runtime
+        }
         double area = length * width;
         return area;
+
     }
+
+    public static String explainArea(String language) {
+        switch (language) {
+            case "English":
+            return "Area equals length * width";
+            case "French":
+            return "La surface est egale a la longueur * la largeur";
+            case "Spanish":
+            return "area es igual a largo * ancho";
+            default:
+            return "Language not available";
+        }
+
+    }
+
+    public static void printArea(double length, double width, double area) {
+        System.out.println("A rectangle with a length of " + length + " and a width of " + width + " has an area of " + area);
+    }
+    
 }
+
---
+++ SECTION FOUR/WorkBook4.3/Weather.java
@@ -0,0 +1,29 @@
+public class Weather {
+    public static void main(String[] args) {
+        double noon = 77;
+        double evening = 61;
+        double midnight = 55;
+        printTemperatures(midnight);
+        printTemperatures(evening);
+        printTemperatures(noon);
+
+    }
+    /**
+     * Function name: fahrenheitToCelsius - converts fahrenheit to celcius
+     * @param fahrenheit (double)
+     * @return celcius (double)
+     */
+    public static double fahrenheitToCelsius (double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
+    }
+
+    /**
+     * Name: printTemperatures - prints a temperature in fahrenheit and celcius
+     * @param fahrenheit (double)
+     */
+    public static void printTemperatures(double fahrenheit) {
+        System.out.println("F: " + fahrenheit);
+        System.out.println("C: " + fahrenheitToCelsius(fahrenheit) + "\n");
+        
+    }
+}
