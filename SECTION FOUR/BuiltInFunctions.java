c29bcb0a8e4fafbad4e5692d7b18dd24ff94f55a
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-17 13:44:37 +0300

    git rm .class

---
+++ SECTION FOUR/BuiltInFunctions.java
@@ -0,0 +1,105 @@
+public class BuiltInFunctions {
+
+    public static void main(String[] args) {
+
+        /**
+         * Scenario 1
+         * 
+         * Find a built-in function that prints the message on the console.
+         * @param message (String)
+         */
+        String message = "Hello, World!";
+        System.out.println(message);
+
+        /**
+         * Scenario 2
+         * 
+         * Find a built-in function that returns the length of a string.
+         * @return length (int)
+         */
+        String example = "Java";
+        int length = example.length();
+        System.out.println(length);
+
+        /**
+         * Scenario 3
+         * 
+         * Find a built-in function that converts a string to lowercase.
+         * @return lowerCase (String)
+         */
+        String upperCase = "JAVA";
+        String lowercase = upperCase.toLowerCase();
+        System.out.println(lowercase);
+
+        /**
+         * Scenario 4
+         * 
+         * Find a built-in function that checks if a string starts with a specified prefix.
+         * @param prefix (String)
+         * @return startsWithPrefix (boolean)
+         */
+        String programming = "Java programming";
+        boolean doesStartWithPrefix = programming.startsWith("Java");
+        System.out.println("Does it start with prefix? " + doesStartWithPrefix);
+
+
+        /**
+         * Scenario 5
+         * 
+         * Find a built-in function that replaces all occurrences of a specified character in a string with another character.
+         * @param oldChar (char)
+         * @param newChar (char)
+         * @return replacedString (String)
+         */
+        String original = "Java is fun!";
+        String updated = original.replace('a', 'o');
+        System.out.println(updated);
+        
+        /**
+         * Scenario 6
+         * 
+         * Find a built-in function that calculates the square root of a number.
+         * @param number (double)
+         * @return squareRoot (double)
+         */
+        
+        double number = 9;
+        double squareRoot = Math.sqrt(number);
+        System.out.println(squareRoot);
+
+        /**
+         * Scenario 7
+         * 
+         * Find a built-in function that calculates the power of a number.
+         * @param base (double)
+         * @param exponent (double)
+         * @return power (double)
+         */
+        double base = 2;
+        double exponent = 3;
+        System.out.println(Math.pow(base, exponent));
+
+        /**
+         * Scenario 8
+         * 
+         * Find a built-in function that generates a random number between 0.0 (inclusive) and 1.0 (exclusive).
+         * @return randomNumber (double)
+         */
+        double randomDecimal = Math.random();
+        System.out.println(randomDecimal);
+
+        /**
+         * Scenario 9
+         * 
+         * Find a built-in function that returns the larger of two numbers.
+         * @param number1 (int)
+         * @param number2 (int)
+         * @return maxNumber (int)
+         */
+        int number1 = 5;
+        int number2 = 10;
+        int maxNumber = Math.max(number1, number2);
+        System.out.println(maxNumber);
+
+    }
+}
