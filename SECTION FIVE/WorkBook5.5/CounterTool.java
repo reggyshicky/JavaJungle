a7c6733cfd3c34413833912fb639c98d0d37997b
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-20 01:21:30 +0300

    userinput driven program, more logic problems on for loops

---
+++ SECTION FIVE/WorkBook5.5/CounterTool.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class CounterTool {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("I hear you like to count by threes");
+        String ans = scan.nextLine();
+
+        System.out.println("\nOh, Ok...");
+        System.out.println("\tPick a number to count by: ");
+        int interval = scan.nextInt();
+        System.out.println("\tPick a number to start counting from: ");
+        int start = scan.nextInt();
+        System.out.println("\tPick a number to count to: ");
+        int end = scan.nextInt();
+
+        for (int i = start; i <= end; i += interval) {
+            System.out.print(i + " ");
+        }
+
+        scan.close();
+    }
+}
