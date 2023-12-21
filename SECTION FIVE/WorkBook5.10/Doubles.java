7b37a2c2c4268daa9411a8f447d7f64f8eb07bfc
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-21 17:55:25 +0300

    Math.random() * 6 + 1

--- SECTION FIVE/WorkBook5.10/Doubles.java
+++ SECTION FIVE/WorkBook5.10/Doubles.java
@@ -1,6 +1,18 @@
 public class Doubles {
     public static void main(String[] args) {
+        int dice1 = rollDice();
+        int dice2 = rollDice();
 
+        System.out.println("Dice 1: " + dice1);
+        System.out.println("Dice 2: " + dice2 + "\n");
+
+        while (dice1 != dice2) {
+            dice1 = rollDice();
+            dice2 = rollDice();
+            System.out.println("\nDice 1: " + dice1);
+            System.out.println("Dice 2: " + dice2 + "\n");
+        }
+        System.out.println("You rolled doubles");
     }
 
     /**
@@ -8,7 +20,7 @@ public class Doubles {
      * @return randomNumber (int)
      */
     public static int rollDice() {
-        int randomNumber = (int) Math.random() * 6;
+        int randomNumber = (int) (Math.random() * 6) + 1;
         return randomNumber;
     }
 }
