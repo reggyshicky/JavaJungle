de3283239dac2f754d20f6ff05e0aa4435e48b14
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-21 23:51:40 +0300

    Math.random() * 6 + 1

---
+++ SECTION FIVE/BreakAndContinueExamples.java
@@ -0,0 +1,23 @@
+public class BreakAndContinueExamples {
+
+    public static void main(String[] args) {
+        // Example 1: Print numbers from 1 to 10, but stop when the number 5 is reached (using break)
+        // TODO: Write a loop that prints numbers from 1 to 10, but stops when the number 5 is reached using the 'break' statement
+
+        // TODO: Write a loop that prints numbers from 1 to 10, but skips the number 5 using the 'continue' statement
+        for (int i = 1; i < 11; i++) {
+            if (i == 5) {
+                continue;
+            }
+            System.out.println(i);
+        }
+        // Example 3: Find the smallest number greater than 20 that is divisible by 3 (using break)
+        // TODO: Write a loop that finds the smallest number greater than 20 that is divisible by 3 using the 'break' statement
+
+        // TODO: Write a loop that prints all even numbers from 1 to 20, skipping odd numbers using the 'continue' statement
+
+        for (int i = 1; i <= 20; i++) {
+            
+        }
+    }
+}
