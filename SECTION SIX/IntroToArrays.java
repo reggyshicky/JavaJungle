a48237951bbcbf5bdd8cfe4f360a46ab3ec400d4
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-23 12:13:51 +0300

    introduction to arrays

---
+++ SECTION SIX/IntroToArrays.java
@@ -0,0 +1,11 @@
+public class IntroToArrays {
+    public static void main(String[] args) {
+        String[] kingdoms = {"Mercia", "Wessex", "Northumbria", "East Anglia"};
+        System.out.println(kingdoms); //prints hashcode representation of the reference/ address
+        System.out.println(kingdoms[0]);
+        System.out.println(kingdoms[1]);
+        System.out.println(kingdoms[2]);
+        System.out.println(kingdoms[3]);
+        System.out.println(kingdoms[4]); //out error si raised
+    }
+}
