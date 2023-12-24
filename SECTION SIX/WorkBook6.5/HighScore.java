94079db930f61f7aea1534d4aea1e85a1c24dd05
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-25 00:52:31 +0300

    logic to compare your scores

---
+++ SECTION SIX/WorkBook6.5/HighScore.java
@@ -0,0 +1,22 @@
+public class HighScore {
+    public static void main(String[] args) {
+        int[] scores = {randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber()};
+        int highScore = 0;
+        System.out.println("Here are the scores!");
+        for (int i = 0; i < scores.length; i++) {
+            System.out.print(scores[i] + " ");
+            if (scores[i] > highScore) {
+                highScore = scores[i];
+            }
+        }
+        System.out.println("\n\nThe highest score is: " + highScore + ". Give that man a cookie!");
+
+         
+    }
+
+    //returns a random number between 0 to 49999
+    public static int randomNumber() {
+        int randomNo = (int) (Math.random() * 49999) + 1;
+        return randomNo;
+    }
+}
