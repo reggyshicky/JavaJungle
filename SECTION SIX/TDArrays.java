0b07e2afd63e426c1e63d022d238a6b7189601a0
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-25 17:36:33 +0300

    2D array

---
+++ SECTION SIX/TDArrays.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class TDArrays {
+    public static void main(String[] args) {
+        
+        //easier way to initialize an array
+        int[][] grades = {
+            {72, 74, 78, 76},
+            {65, 64, 61, 67},
+            {95, 98, 99, 100}
+        };
+        //int[][] grades = new int[3][4];
+        // grades[0][0] = 72;
+        // grades[0][1] = 74;
+        // grades[0][2] = 78;
+        // grades[0][3] = 76;
+
+        // grades[1][0] = 65;
+        // grades[1][1] = 64;
+        // grades[1][2] = 61;
+        // grades[1][3] = 67;
+
+        // grades[2][0] = 95;
+        // grades[2][1] = 98;
+        // grades[2][2] = 99;
+        // grades[2][3] = 100;
+         
+        System.out.println("\tHarry: " + grades[0][0] + " " + grades[0][1] + " " + grades[0][2] + " " + grades[0][3]);
+        System.out.println("\tRon: " + grades[1][0] + " " + grades[1][1] + " " + grades[1][2] + " " + grades[1][3]);
+        System.out.println("\tHermione: " + grades[2][0] + " " + grades[2][1] + " " + grades[2][2] + " " + grades[2][3]);
+    }
+}
