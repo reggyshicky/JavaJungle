51081e9e827cb71903e9d8e3896097df513891db
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-26 00:54:04 +0300

    2D array manipulation

--- SECTION SIX/TDArrays.java
+++ SECTION SIX/TDArrays.java
@@ -9,24 +9,17 @@ public class TDArrays {
             {65, 64, 61, 67},
             {95, 98, 99, 100}
         };
-        //int[][] grades = new int[3][4];
-        // grades[0][0] = 72;
-        // grades[0][1] = 74;
-        // grades[0][2] = 78;
-        // grades[0][3] = 76;
-
-        // grades[1][0] = 65;
-        // grades[1][1] = 64;
-        // grades[1][2] = 61;
-        // grades[1][3] = 67;
-
-        // grades[2][0] = 95;
-        // grades[2][1] = 98;
-        // grades[2][2] = 99;
-        // grades[2][3] = 100;
-         
-        System.out.println("\tHarry: " + grades[0][0] + " " + grades[0][1] + " " + grades[0][2] + " " + grades[0][3]);
-        System.out.println("\tRon: " + grades[1][0] + " " + grades[1][1] + " " + grades[1][2] + " " + grades[1][3]);
-        System.out.println("\tHermione: " + grades[2][0] + " " + grades[2][1] + " " + grades[2][2] + " " + grades[2][3]);
+        //grades.length - numbers of rows
+        for (int i = 0; i < grades.length; i++) {
+            switch (i) {
+                case 0: System.out.print("\tHarry: "); break;
+                case 1: System.out.print("\tRon: "); break;
+                case 2: System.out.print("\tHermione: "); break;
+            }
+            for (int j = 0; j < grades[i].length; j++) {
+                System.out.print(grades[i][j] + " ");
+            }
+            System.out.println(); //prints a new line
+        }
     }
 }
