d970885c7888f3b1d51d7ff34d789170b88b1a98
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-26 01:54:18 +0300

    number of rows is the length of an array, columns is the number of elements in an row

--- SECTION SIX/WorkBook6.13/Prices.java
+++ SECTION SIX/WorkBook6.13/Prices.java
@@ -1,31 +1,29 @@
 public class Prices {
     public static void main(String[] args) {
         double[][] prices = {
-            {12.99, 8.99, 9.99, 10.49, 11.99},
-            {0.99, 1.99, 2.49, 1.49, 2.99},
-            {8.99, 7.99, 9.49, 9.99, 10.99}
+                { 12.99, 8.99, 9.99, 10.49, 11.99 },
+                { 0.99, 1.99, 2.49, 1.49, 2.99 },
+                { 8.99, 7.99, 9.49, 9.99, 10.99 }
         };
-        // double[][] prices = new double[3][5];
-        // prices[0][0] = 12.99;
-        // prices[0][1] = 8.99;
-        // prices[0][2] = 9.99;
-        // prices[0][3] = 10.49;
-        // prices[0][4] = 11.99;
+        for (int i = 0; i < prices.length; i++) {
+            switch (i) {
+                case 0:
+                    System.out.print("Baking: ");
+                    break;
+                case 1:
+                    System.out.print("Beverage: ");
+                    break;
+                case 2:
+                    System.out.print("Cereals: ");
+                    break;
 
-        // prices[1][0] = 0.99;
-        // prices[1][1] = 1.99;
-        // prices[1][2] = 2.99;
-        // prices[1][3] = 1.49;
-        // prices[1][4] = 2.99;
+            }
+            for (int j = 0; j < prices[i].length; j++) {
+                System.out.print(prices[i][j] + " ");
+            }
+            System.out.print("\n");
 
-        // prices[2][0] = 8.99;
-        // prices[2][1] = 7.99;
-        // prices[2][2] = 9.49;
-        // prices[2][3] = 10.49;
-        // prices[2][4] = 11.99;
-        System.out.println("Baking: " + prices[0][0] + " " + prices[0][1] + " " + prices[0][2] + " " + prices[0][3]);
-        System.out.println("Beverage: " + prices[1][0] + " " + prices[1][1] + " " + prices[1][2] + " " + prices[1][3]);
-        System.out.println("Cereals: " + prices[2][0] + " " + prices[2][1] + " " + prices[2][2] + " " + prices[2][3]);
+        }
 
     }
 }
