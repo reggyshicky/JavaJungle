f63cbdbcfa85df63fa2c6e0c730d47026fbddcf5
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-27 23:53:39 +0300

    created two methods leveraging on Math.random()

--- SECTION SEVEN/WorkBook7.5/Main.java
+++ SECTION SEVEN/WorkBook7.5/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Person person = new Person("Rayan Slim", "Canadian", "0/01/111", 5);
-        ////
+        /////
         person.chooseSeat();
         System.out.println("Name: " + person.getName() + "\n" + "Nationality: " + person.getNationality() + "\n"
                 + "Date of Birth: " + person.getDateOfBirth() + "\n" + "Seat Number: " + person.getSeatNumber() + "\n");
