3b17147fb832045bc2521ebd34154602b4583567
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-27 14:57:54 +0300

    setters are used to change value of a private field -Encapsulation

--- SECTION SEVEN/Car.java
+++ SECTION SEVEN/Car.java
@@ -11,18 +11,40 @@ public class Car {
         this.color = color;
     }
 
-    //return type of the getter depends on a particular field
+    // return type of the getter depends on a particular field
     public String getMake() {
         return make;
     }
+
     public double getPrice() {
         return price;
     }
-    public int getYear () {
+
+    public int getYear() {
         return year;
     }
-    public String getColor () {
+
+    public String getColor() {
         return color;
     }
-}
 
+    /*
+     * So notice here we do need the this keyword to distinguish between the field
+     * and the parameter that gets passed in.
+     * Whereas in the getter there are no parameters.
+     * So make will automatically refer to the object as field.
+     */
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
