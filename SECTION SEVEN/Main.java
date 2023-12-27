c182b9e8d59ae9b0de71ef2b7c42904c4abf1073
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-27 11:08:33 +0300

    use of a constructor, a constructor is used to initialize an object when it is created, a constructor updates the field names of an object with the  parameters, this keyword is used to refer to the current object being created

--- SECTION SEVEN/Main.java
+++ SECTION SEVEN/Main.java
@@ -1,16 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Car nissan = new Car();
-        nissan.make = "Nissan";
-        nissan.price = 10000;
-        nissan.year = 2020;
-        nissan.color = "Green";
-
-        Car dodge = new Car();
-        dodge.make = "dodge";
-        dodge.price = 11000;
-        dodge.year = 2019;
-        dodge.color = "Blue";
+        Car nissan = new Car("Nissan", 10000, 2020, "Green");
+    
+        Car dodge = new Car("dodge", 11000, 2019, "Blue");
+       
 
         System.out.println("This " + nissan.make + " is worth $" + nissan.price +
                 ". It was built in " + nissan.year + ". It is " + nissan.color + ".\n");
