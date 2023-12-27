c3cfa5b9da348b8354e63db94c1d5e349ef56958
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-27 14:00:54 +0300

    use of getters, getters ared used to retrieve private attributes, used in encapsulation - data integrity

--- SECTION SEVEN/Main.java
+++ SECTION SEVEN/Main.java
@@ -1,13 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Car nissan = new Car("Nissan", 10000, 2020, "Green");
-    
         Car dodge = new Car("dodge", 11000, 2019, "Blue");
-       
 
-        System.out.println("This " + nissan.make + " is worth $" + nissan.price +
-                ". It was built in " + nissan.year + ". It is " + nissan.color + ".\n");
-        System.out.println("This " + dodge.make + " is worth $" + dodge.price +
-                ". It was built in " + dodge.year + ". It is " + dodge.color + ".\n");
+        System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() +
+                ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
+        System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() +
+                ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
     }
 }
