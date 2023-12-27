59247fcf5a3d4657912f44c31189d380c612f56a
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-27 15:08:26 +0300

    generating setters and getting using java code generators - Visual Studio

--- SECTION SEVEN/Car.java
+++ SECTION SEVEN/Car.java
@@ -11,40 +11,36 @@ public class Car {
         this.color = color;
     }
 
-    // return type of the getter depends on a particular field
     public String getMake() {
-        return make;
+        return this.make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
     }
 
     public double getPrice() {
-        return price;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    /*
-     * So notice here we do need the this keyword to distinguish between the field
-     * and the parameter that gets passed in.
-     * Whereas in the getter there are no parameters.
-     * So make will automatically refer to the object as field.
-     */
-    public void setMake(String make) {
-        this.make = make;
+        return this.price;
     }
 
     public void setPrice(double price) {
         this.price = price;
     }
+
+    public int getYear() {
+        return this.year;
+    }
+
     public void setYear(int year) {
         this.year = year;
     }
+
+    public String getColor() {
+        return this.color;
+    }
+
     public void setColor(String color) {
         this.color = color;
     }
+
 }
