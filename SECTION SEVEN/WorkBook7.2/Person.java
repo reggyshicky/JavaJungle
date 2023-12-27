6694ef0cae1cb774750a8239c0d70f5dc7fc7365
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-27 11:43:16 +0300

    use of constructors in java OOP

---
+++ SECTION SEVEN/WorkBook7.2/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    String name;
+    String nationality;
+    String dateOfBirth;
+    int seatNumber;
+
+    public Person (String name, String nationality, String dateOfBirth, int seatNumber) {
+        this.name = name;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+        this.seatNumber = seatNumber;
+    }
+}
