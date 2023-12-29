0b89c86d47c6cd65c89412409dac0644fee906e7
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-29 12:46:03 +0300

    LocalDate.now()

---
+++ SECTION SEVEN/World of Objects/Part 2/Contact.java
@@ -0,0 +1,59 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class Contact {
+    private String name;
+    private String phoneNumber;
+    private String birthDate;
+    private int age;
+
+    // initializes the fields of a newly created object
+    public Contact(String name, String phoneNumber, String birthDate) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
+        this.age = toAge(birthDate);
+    }
+
+    // copy constructor - initializes a new object using an existing object's field
+    public Contact(Contact source) {
+        this.name = source.name;
+        this.phoneNumber = source.phoneNumber;
+        this.birthDate = source.birthDate;
+        this.age = source.age;
+    }
+
+    private int toAge(String birthDate) {
+        LocalDate parsedBirthDate = LocalDate.parse(birthDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate currentDate = LocalDate.now();
+        Period period = Period.between(parsedBirthDate, currentDate);
+        return period.getYears();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+        setAge(toAge(birthDate));
+    }
+
+    private void setAge(int age) {
+        this.age = age;
+    }
+}
