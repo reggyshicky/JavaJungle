f58e73a4a78d0d9f27c096c089174e087494885e
Reggy Shicky <reginahshikanda@gmail.com>
2023-12-31 19:58:25 +0300

    Springboot MVC Design

---
+++ SpringWebApplications/work-book/work-book/src/main/java/com/workbook21/workbook/WorkBookApplication.java
@@ -0,0 +1,13 @@
+package com.workbook21.workbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkBookApplication.class, args);
+	}
+
+}
