d74de017c664885ebef0232400cbdde11efa910c
Reggy Shicky <reginahshikanda@gmail.com>
2024-01-04 12:15:16 +0300

    added data to the model

--- SpringWebApplications/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ SpringWebApplications/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,11 +1,14 @@
 package com.ltp.gradesubmission;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
     @GetMapping("/grades")
-    public String getGrades() {
+    public String getGrades(Model model) {
+        Grade grade = new Grade("Reginah", "Mathematics", "A");
+        model.addAttribute("grade", grade);
         return "grades";
     }
     
