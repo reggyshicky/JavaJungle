0d35623055e3f9b7c3b030013d7485f20ea53401
Reggy Shicky <reginahshikanda@gmail.com>
2024-01-14 09:22:40 +0300

    thymeleaf loop -each

--- SpringWebApplications/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ SpringWebApplications/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,14 +1,22 @@
 package com.ltp.gradesubmission;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
+    List<Grade> studentGrades = Arrays.asList(
+        new Grade("Reginah", "Mathematics", "A"),
+        new Grade("Shicky", "Physics", "A"),
+        new Grade("Reggy", "Statistics", "A")
+    );
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        Grade grade = new Grade("Reginah", "Mathematics", "A");
-        model.addAttribute("grade", grade);
+        model.addAttribute("grades", studentGrades);
         return "grades";
     }
     
