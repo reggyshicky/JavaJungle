e88c09cadbdefced3c8dd2d2bd82af9350f848d3
Reggy Shicky <reginahshikanda@gmail.com>
2024-01-14 19:04:54 +0300

    form submission

--- SpringWebApplications/grade-submission - Creating a Form/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ SpringWebApplications/grade-submission - Creating a Form/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -6,18 +6,28 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GradeController {
-    List<Grade> studentGrades = Arrays.asList(
-        new Grade("Reginah", "Mathematics", "A"),
-        new Grade("Shicky", "Physics", "A"),
-        new Grade("Reggy", "Statistics", "A")
-    );
+    List<Grade> studentGrades = new ArrayList<>();
+    
     @GetMapping("/")
     public String gradeForm(Model model) {
-        model.addAttribute("grade", new Grade("Reginah", "Mathematics", "A"));
+        model.addAttribute("grade", new Grade());
         return "form";
     }
+
+    @PostMapping("/handleSubmit")
+    public String submitForm(Grade grade) {
+        studentGrades.add(grade);
+        return "redirect:/grades";
+    }
+    @GetMapping("/grades")
+    public String getGrades(Model model) {
+        model.addAttribute("grades", studentGrades);
+        return "grades";
+    }
+
 
 }
