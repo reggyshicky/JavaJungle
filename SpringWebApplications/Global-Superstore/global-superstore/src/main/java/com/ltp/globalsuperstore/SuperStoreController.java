e1838be9b4cbab70d8068e1ff9645e55e05165cc
Reggy Shicky <reginahshikanda@gmail.com>
2024-01-15 15:35:55 +0300

    controllers, th:each, th:value

---
+++ SpringWebApplications/Global-Superstore/global-superstore/src/main/java/com/ltp/globalsuperstore/SuperStoreController.java
@@ -0,0 +1,20 @@
+package com.ltp.globalsuperstore;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SuperStoreController {
+    @GetMapping("/")
+    public String getFrom(Model model) {
+        model.addAttribute("categories", Constants.CATEGORIES);
+        return "form";
+    }
+
+    @GetMapping("/inventory")
+    public String getInventory() {
+        return "inventory";
+    }
+
+}
