365484f8f68e6db5f3fceaf2e6e092dbbf937f66
Reggy Shicky <reginahshikanda@gmail.com>
2024-01-16 12:34:48 +0300

    postmapping

--- SpringWebApplications/Global-Superstore/global-superstore/src/main/java/com/ltp/globalsuperstore/SuperStoreController.java
+++ SpringWebApplications/Global-Superstore/global-superstore/src/main/java/com/ltp/globalsuperstore/SuperStoreController.java
@@ -1,15 +1,28 @@
 package com.ltp.globalsuperstore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SuperStoreController {
+    private List<Item> items = new ArrayList<Item>();
+
     @GetMapping("/")
     public String getFrom(Model model) {
+        model.addAttribute("item", new Item());
         model.addAttribute("categories", Constants.CATEGORIES);
         return "form";
+    }
+
+    @PostMapping("/submitItem")
+    public String handleSubmit(Item item) {
+        items.add(item);
+        return "redirect:/inventory";
     }
 
     @GetMapping("/inventory")
