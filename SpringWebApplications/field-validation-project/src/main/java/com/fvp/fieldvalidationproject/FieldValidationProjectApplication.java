7716874361673636bfeddb106b3df8275ea573ee
Reggy Shicky <reginahshikanda@gmail.com>
2024-01-17 21:36:14 +0300

    field validation

---
+++ SpringWebApplications/field-validation-project/src/main/java/com/fvp/fieldvalidationproject/FieldValidationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fvp.fieldvalidationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldValidationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldValidationProjectApplication.class, args);
+	}
+
+}
---
+++ SpringWebApplications/field-validation-project/src/test/java/com/fvp/fieldvalidationproject/FieldValidationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fvp.fieldvalidationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldValidationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
