1610f7ce4558257b0997228ff32cee07d622cd8e
Reggy Shicky <reginahshikanda@gmail.com>
2024-01-28 22:27:13 +0300

    creating packages for controllers and services

--- SpringWebApplications/Microservices/QuizApp/QuizApp/src/main/java/com/microservices/QuizApp/controller/QuestionController.java
+++ SpringWebApplications/Microservices/QuizApp/QuizApp/src/main/java/com/microservices/QuizApp/controller/QuestionController.java
@@ -1,4 +1,4 @@
-package com.microservices.QuizApp;
+package com.microservices.QuizApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuestionController {
     @GetMapping("allQuestions")
     public String getAllQuestions() {
-        return "Hi, These are your questions";
+        return QuestionService.getAllQuestion();
     }
 }
---
+++ SpringWebApplications/Microservices/QuizApp/QuizApp/src/main/java/com/microservices/QuizApp/service/QuestionService.java
@@ -0,0 +1,4 @@
+package com.microservices.QuizApp.service;
+
+public class QuestionService {
+}
