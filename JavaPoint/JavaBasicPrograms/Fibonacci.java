0862554f756b6af9a7365fdf245fc4ab7c1424ab
reggyshicky <reginahshikanda@gmail.com>
2024-03-10 12:39:20 +0300

    fibonacci series with recursion and without recursion

--- JavaPoint/JavaBasicPrograms/Fibonacci.java
+++ JavaPoint/JavaBasicPrograms/Fibonacci.java
@@ -4,8 +4,9 @@ public class Fibonacci {
     //There are two ways to write fibonacci i)Fibonacci series in without using  recursion
     //ii) Fibonacci series using recursion
 
-    //Without using recursion
+    
     public static void main(String[] args) {
+        //Fibonacci series Without using recursion
         int n1=0, n2=1, n3, i, count=10;
         System.out.print(n1+" "+n2); //printing 0 and 1
 
@@ -15,6 +16,35 @@ public class Fibonacci {
              n1 = n2;
              n2 = n3;
         }
+
+        //ALTERNATIVE
+        // int n = 10; // Number of Fibonacci numbers to generate
+        // System.out.println("Fibonacci Series:");
+        // int prev = 0;
+        // int next = 1;
+        // for (int i = 0; i < n; i++) {
+        //     System.out.print(prev + " ");
+        //     int sum = prev + next;
+        //     prev = next;
+        //     next = sum;
+        // }
+
+        System.out.println("\n\n");
+        //Fibonacci series using recursion
+        int n = 20; //Number of Fibonacci numbers to generate
+        System.out.println("Fibonacci Series:");
+        for (int j = 0; j < n; j ++) {
+            System.out.print(fibonacci(j) + " ");
+        }
     
+    }
+
+    //Fibonacci series using recursion
+    public static int fibonacci(int n) {
+        if (n <= 1) { //n in the fibonacci func represent the pos in the fibonacci sequence for which we want to calculate the value. ie if n is 0, we calculate the value of fibonacci number at pos 0 in the sequence
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n -2);
+        }
     }
 }
