13c331306d4621231725ecdf8ac8f76468491f77
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2025-06-15 15:44:32 +0300

    primitive data types in java

---
+++ Java FundaMentals/src/PrimitiveDataTypes.java
@@ -0,0 +1,5 @@
+public class PrimitiveDataTypes {
+    /* Use Integer.MAX_VALUE and Integer.MIN_VALUE to get the maximum
+       and miminum number you can assign to an int. (Wrapper class: Integer)
+     */
+}
