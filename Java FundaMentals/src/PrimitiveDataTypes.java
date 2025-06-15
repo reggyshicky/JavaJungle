14e0a181f63d6a75c22598abff41b10758fbae4a
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2025-06-15 16:01:25 +0300

    using the Integer wrapper class to get MIN_VALUE and MAX_VALUE of an int

--- Java FundaMentals/src/PrimitiveDataTypes.java
+++ Java FundaMentals/src/PrimitiveDataTypes.java
@@ -2,4 +2,16 @@ public class PrimitiveDataTypes {
     /* Use Integer.MAX_VALUE and Integer.MIN_VALUE to get the maximum
        and miminum number you can assign to an int. (Wrapper class: Integer)
      */
+
+    static int myMaxIntValue = Integer.MAX_VALUE;
+    static int myMinValue = Integer.MIN_VALUE;
+
+
+   public static void main(String[] args) {
+       System.out.println("Minimum value is " + myMinValue);
+       //-21,474,836,648
+       System.out.println("Maximum value is " + myMaxIntValue);
+       //21,474,836,647
+   }
+
 }
